a5957616d1d7ac94f44a5dd9b593da23762969e7
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-12-18 15:03:12 -0500

    Commit

---
+++ myApp.js
@@ -0,0 +1,54 @@
+let express = require('express');
+let app = express();
+
+console.log('Hello World')
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/json', function(req, res) {
+  let message = { "message": "Hello json" }
+  if(process.env.MESSAGE_STYLE == 'uppercase') {
+    message.message = message.message.toUpperCase();
+  }
+  res.json(message);
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
