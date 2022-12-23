27d67b8be8daf4808692c46e5907e9110af4c631
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-12-23 16:44:34 -0500

    Done

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var express = require('express');
 var cors = require('cors');
+const fileUpload = require('express-fileupload')
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
+app.use(fileUpload())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function (req, res) {
+  if (!req.files) {
+    res.status(400).send("No files were uploaded.");
+  }
 
+  const file = req.files.upfile
+
+  res.json({
+    name: file.name,
+    type: file.mimetype,
+    size: file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
