d0df708fbe3a889e412c4b603391558f5a6444f1
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-12-24 00:20:37 -0500

    Logs

--- controllers/exercise.controller.js
+++ controllers/exercise.controller.js
@@ -2,7 +2,39 @@ const User = require('../models/User')
 const Exercise = require('../models/Exercise')
 
 const getLogs = (req, res) => {
+    User.findById(req.params._id, (err, doc) => {
+        if (err) res.sendStatus(500)
 
+        let found = Exercise.find({
+            username: doc.username,
+        })
+
+        if (req.query.limit) found.limit(parseInt(req.query.limit))
+
+        found.exec((err, result) => {
+            if (err) res.sendStatus(500)
+
+            if (req.query.from && req.query.to) {
+                result = result.filter(el => {
+                    let date = new Date(el.date).toISOString().slice(0, 10)
+                    return date >= req.query.from && date <= req.query.to
+                })
+            }
+
+            res.send({
+                username: doc.username,
+                count: result.length,
+                _id: doc._id,
+                log: result.map(el => {
+                    return {
+                        description: el.description,
+                        duration: el.duration,
+                        date: el.date
+                    }
+                })
+            })
+        })
+    })
 }
 
 const createExercise = (req, res) => {
